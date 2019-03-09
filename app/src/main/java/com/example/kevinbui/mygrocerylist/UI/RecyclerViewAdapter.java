7a8a1f07d5942073439fa987c736dd034657cfd4
buikevin96 <buikevin96@gmail.com>
2019-03-09 10:34:25 -0500

    Added methods to RecyclerViewAdapter

--- app/src/main/java/com/example/kevinbui/mygrocerylist/UI/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/kevinbui/mygrocerylist/UI/RecyclerViewAdapter.java
@@ -1,5 +1,100 @@
 package com.example.kevinbui.mygrocerylist.UI;
 
-public class RecyclerViewAdapter {
-    
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.kevinbui.mygrocerylist.Model.Grocery;
+import com.example.kevinbui.mygrocerylist.R;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private Context context;
+    private List<Grocery> groceryItems;
+
+    public RecyclerViewAdapter(Context context, List<Grocery> groceryItems) {
+        this.context = context;
+        this.groceryItems = groceryItems;
+    }
+
+
+    // Create View by inflating list_row and return it and pass in view and context
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        // View has list row ready to be set
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
+
+        return new ViewHolder(view, context);
+    }
+
+    // Where everything is bind together
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
+
+        Grocery grocery = groceryItems.get(position); // Contextualizing grocery item
+
+        holder.groceryItemName.setText(grocery.getName()); // Binding things to our view
+        holder.quantity.setText(grocery.getQuantity());
+        holder.dateAdded.setText(grocery.getDateItemAdded());
+    }
+
+    @Override
+    public int getItemCount() {
+        return groceryItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView groceryItemName;
+        public TextView quantity;
+        public TextView dateAdded;
+        public Button editButton;
+        public Button deleteButton;
+        public int id; // Holding ID of each item
+
+        public ViewHolder(@NonNull View view, Context ctx) {
+
+            super(view);
+
+            context = ctx;
+
+            // Set up widgets
+            groceryItemName = (TextView) view.findViewById(R.id.name);
+            quantity = (TextView) view.findViewById(R.id.quantity);
+            dateAdded = (TextView) view.findViewById(R.id.dateAdded);
+
+            editButton = (Button) view.findViewById(R.id.editButton);
+            deleteButton = (Button) view.findViewById(R.id.deleteButton);
+
+            editButton.setOnClickListener(this);
+            deleteButton.setOnClickListener(this);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Go to Next Screen
+
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.editButton:
+
+                    break;
+                case R.id.deleteButton:
+
+                    break;
+            }
+
+        }
+    }
 }
