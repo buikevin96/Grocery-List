ba8db6e6ccbbe617a186cd155a995ac88a071cb9
buikevin96 <buikevin96@gmail.com>
2019-03-13 09:57:24 -0400

    updated DetailsActivity

--- app/src/main/java/com/example/kevinbui/mygrocerylist/Activity/DetailsActivity.java
+++ app/src/main/java/com/example/kevinbui/mygrocerylist/Activity/DetailsActivity.java
@@ -2,14 +2,34 @@ package com.example.kevinbui.mygrocerylist.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.kevinbui.mygrocerylist.R;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private TextView itemName;
+    private TextView quantity;
+    private TextView dateAdded;
+    private int groceryId;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        itemName = (TextView)findViewById(R.id.itemNameDet);
+        quantity = (TextView)findViewById(R.id.quantityDet);
+        dateAdded = (TextView)findViewById(R.id.dateAddedDet);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            itemName.setText(bundle.getString("name"));
+            quantity.setText(bundle.getString("quantity"));
+            dateAdded.setText(bundle.getString("date"));
+            groceryId = bundle.getInt("id");
+        }
+
     }
 }
